f50bfead78f494c4909c34314838fb921af5319d
cathySyu <siangjyun0926@gmail.com>
2022-05-08 14:55:53 +0800

    sum.js

--- Basic/sum.js
+++ Basic/sum.js
@@ -1,10 +1,27 @@
 // 回傳 1 + 2 + ... + n 的結果
 
-function sum(n){
-    return n*(n+1)/2;
-}
+// 寫法 1
+// function sum(n){
+//     return n*(n+1)/2;
+// }
 
-console.log(sum(1));   // 1
-console.log(sum(2));   // 3
-console.log(sum(10));  // 55
-console.log(sum(100)); // 5050
+// console.log(sum(1));   // 1
+// console.log(sum(2));   // 3
+// console.log(sum(10));  // 55
+// console.log(sum(100)); // 5050
+
+// 寫法 2
+// function sum(n){
+//     let i = "";
+//     for(i=0; n>=i; i++){    
+//     }
+//     return i*(i-1)/2;
+// }
+
+// console.log(sum(1));   // 1
+// console.log(sum(2));   // 3
+// console.log(sum(10));  // 55
+// console.log(sum(100)); // 5050
+
+
+// 寫法 3
