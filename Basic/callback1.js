2fb07b7cd2f1bd8815f2832e649012fda19b7237
Cathy <=>
2022-05-14 15:18:37 +0800

    add callback1.js

---
+++ Basic/callback1.js
@@ -0,0 +1,29 @@
+let dt = new Date();
+console.log(`起床了 at ${dt.toISOString()}`);
+
+
+// cb=>callback
+// setTimeout (外包)
+let doWork = function (job, timer, cb) {
+  setTimeout(() => {
+    let dt = new Date();
+    let result = `完成工作: ${job} at ${dt.toISOString()}`;
+    cb(result);
+  }, timer);
+  //return
+  console.log(`在 setTimeout 之後 ${job}`);
+};
+
+// 刷牙 (3000) -> 吃早餐 (5000) -> 寫功課 (3000)
+
+doWork('刷牙', 3000, function (result) {
+  console.log(result);
+  doWork('吃早餐', 5000, function (result) {
+    console.log(result);
+    doWork('寫功課', 3000, function (result) {
+      console.log(result);
+    });
+  });
+});
+
+
