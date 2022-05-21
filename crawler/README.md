91175ee3629d03cb75bca2a0486732a9fa753cf5
Cathy <=>
2022-05-21 14:41:53 +0800

    add

---
+++ crawler/README.md
@@ -0,0 +1,2 @@
+<!-- How to install -->
+cp .env.example .env <!-- cp:copy  env:dotenv  -->
--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -5,14 +5,23 @@
 // 引用進來
 const mysql = require('mysql2/promise');
 
-(async () => {
-  const connection = await mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'nodejs_db',
-  });
+// (async () => {
+//   const connection = await mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'admin',
+//     password: '',
+//     database: 'nodejs_db',
+//   });
+
+  (async () => {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_host,
+      port: process.env.DB_port,
+      user: process.env.DB_user,
+      password: process.env.DB_password,
+      database: process.env.DB_database,
+    });
 
   let [data, fields] = await connection.execute('SELECT * FROM stocks');
   console.log(data);
